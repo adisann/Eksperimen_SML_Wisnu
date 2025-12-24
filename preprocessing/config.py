e2ce335372e61b344bf456f10413fd34f0d40f1a
wisnu.adisan <wisnu.adisan@gmail.com>
2025-12-24 19:49:10 +0700

    Fix Preprocessing: Standardize features to avoid dropna() wiping data

--- preprocessing/config.py
+++ preprocessing/config.py
@@ -2,7 +2,7 @@
 # Konfigurasi fitur untuk SKU tertentu agar proses cepat
 SKU_SPECIFIC_LAGS = {
     216418: [1, 2, 5, 19],
-    216419: [1, 2, 37, 40],
+    216419: [1, 2, 5, 19],
 }
 
 SKU_SPECIFIC_MAS = {
